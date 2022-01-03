befd6262eb513db3592c9aa0afc74269bb623d87
michaellchandra <mchandra05@student.ciputra.ac.id>
2022-01-03 22:36:12 +0800

    add movement player

--- Jodey Fighter Project/Assets/Scripts/PlayerMovement.cs
+++ Jodey Fighter Project/Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,17 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+
+        //Balik Arah Player kiri kanan
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(-4, 4, 1);
+        else if (horizontalInput < 0.01f)
+            transform.localScale = new Vector3(4, 4, 1);
+
 
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
