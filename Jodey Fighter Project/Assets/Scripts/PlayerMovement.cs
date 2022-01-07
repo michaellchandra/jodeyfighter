51fa4059205a25c60a841afd6d96b9b90a9a69d6
deja232 <84335792+deja232@users.noreply.github.com>
2022-01-07 16:03:27 +0700

    healtbar+movement

--- Jodey Fighter Project/Assets/Scripts/PlayerMovement.cs
+++ Jodey Fighter Project/Assets/Scripts/PlayerMovement.cs
@@ -4,48 +4,127 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed; 
+    [SerializeField] private float speed;
+    [SerializeField] private float jump_power;
     private Rigidbody2D body;
+    private Animator animation;
+    private BoxCollider2D boxCollider;
+    private float jumpWallCooldown;
+    private float inputHorizontal;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
 
-    public int MaxHealth = 100;
-    public int currentHealth;
 
-    void start()
-    {
-        currentHealth = MaxHealth;
-    }
 
+
+    //Variabel Rigidbody dan Animator
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        animation = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        inputHorizontal = Input.GetAxis("Horizontal");
 
 
         //Balik Arah Player kiri kanan
-        if (horizontalInput > 0.01f)
-            transform.localScale = new Vector3(-4, 4, 1);
-        else if (horizontalInput < 0.01f)
-            transform.localScale = new Vector3(4, 4, 1);
+        if (inputHorizontal > 0.01f)
+            transform.localScale = new Vector3(-2, 2, 1);
+        else if (inputHorizontal == 0.00f)
+            transform.localScale = new Vector3(-2, 2, 1);
+        else if (inputHorizontal < 0.01f)
+            transform.localScale = new Vector3(2, 2, 1);
 
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        //Parameter Animator
 
-        //if (kenahit)
-       // {
-      //      TakeDamage(10);
-       // }
+        animation.SetBool("running", inputHorizontal != 0);
+        animation.SetBool("grounded", isGrounded());
+
+        //Lompat ke Wall
+
+        if (jumpWallCooldown < 0.2f)
+        {
+
+
+            body.velocity = new Vector2(inputHorizontal * speed, body.velocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+
+
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+
+            else
+            {
+                body.gravityScale = 3;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+                Jump();
+        }
+        else
+        {
+            jumpWallCooldown += Time.deltaTime;
+        }
     }
 
-   // void TakeDamage(int damage)
-  //  {
-  //      currentHealth -= damage;
-   // }
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+
+            body.velocity = new Vector2(body.velocity.x, jump_power);
+            animation.SetTrigger("jump");
+        }
+
+        else if (onWall() && !isGrounded())
+        {
+            if (inputHorizontal == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+            }
+            else
+
+                jumpWallCooldown = 0;
+            body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+            Jump();
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.tag == "Ground")
+        //    grounded = true;
+    }
+
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+        return inputHorizontal == 0 && isGrounded() && !onWall();
+    }
 
 }
